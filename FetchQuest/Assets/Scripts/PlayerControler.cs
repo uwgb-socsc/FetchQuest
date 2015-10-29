6018a1ae74a6f5a468119ba187e95829582a88b9
AsewAsew <burkdt03@uwgb.edu>
2015-10-28 19:58:52 -0500

    Added Character and camera movement scripts
    They're basic, so I'll be working on them more

---
+++ FetchQuest/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerControler : MonoBehaviour {
+
+	private Rigidbody rb;
+	private int count;
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVeritical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVeritical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)	
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) 
+		{
+			winText.text = "You Win!";
+		}
+	}
+}
