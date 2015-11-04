e173d9fb3181302b4735be67de2860506e8a4dab
PepsiDragon <rainick987@yahoo.com>
2015-11-03 20:16:21 -0600

    Did some stuff with Combat and Combatant scripts
    Mostly just pseudo code at this point (incomplete at that). But I've
    figured out what I want to do. Just missing some details to implement it
    entirely, for now.

---
+++ FetchQuest/Assets/Scripts/Combat/Combat.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Combat : MonoBehaviour {
+    /*
+    This class is what makes the Combat actually work.
+    It will put everyone fighting into a list (ordered by who goes first in a turn)
+    Then it calls some kind of option choosing class (either asking the player for input or an AI) 
+    Yes that means this calls for an interface!
+
+    */
+    // List of combatants ordered by turn order
+    // two more lists of allies and enemies. 
+
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void RunCombat()
+    {
+        //while(combatStillGoingOn)
+        /*
+            turnNum++;
+            foreach something in turnOrderList
+                choiceType = something.choose(array/list of enemies)
+                if choiceType == attack
+                    Achoice = something.Achoice()
+                    target = something.chooseTarget(); The target is an actual combatant so just calculate damage and do it
+                    CalcDamage()
+                    target.takeDamage()
+                if choiceType == item
+                    Ichoice = something.Ichoice()
+                    UseItem(Ichoice)
+
+        */
+       
+        
+    }
+}
--- FetchQuest/Assets/Scripts/Combat/Combatant.cs
+++ FetchQuest/Assets/Scripts/Combat/Combatant.cs
@@ -3,8 +3,34 @@ using System.Collections;
 
 public class Combatant : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    /*
+    Charisma
+Stamina
+Intelligence
+Perception
+Agility
+Luck
+Tool Use*
+Dexterity
+Defense
+Strength
+    */
+    double charisma { get; }
+    double stamina { get; }
+    double intelligence { get; }
+    double perception { get; }
+    double agility { get; }
+    double luck { get; }
+    double toolUse { get; }
+    double dexterity { get; }
+    double defense { get; }
+    double strength { get; }
+
+    //a list for attacks
+    //a list for items. 
+    
+    void Start () {
 	
 	}
 	
