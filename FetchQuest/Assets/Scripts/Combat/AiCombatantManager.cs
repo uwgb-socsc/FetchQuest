cb186e5a732977b0b8dfd72d1ec19beeaf1d4125
PepsiDragon <rainick987@yahoo.com>
2015-11-10 13:44:21 -0600

    Added AiCombatantManager and Item scripts
    They still don't actually do anything yet.

---
+++ FetchQuest/Assets/Scripts/Combat/AiCombatantManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AiCombatantManager : ICombatantManager {
+
+    public Attack.Special choose()
+    {
+        throw new System.NotImplementedException();
+    }
+    public Attack Achoice()
+    {
+        throw new System.NotImplementedException();
+    }
+    public Combatant TargetChoice()
+    {
+        throw new System.NotImplementedException();
+    }
+    public Item Ichoice()
+    {
+        throw new System.NotImplementedException();
+    }
+}
