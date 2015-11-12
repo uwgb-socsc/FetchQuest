e26b58441dc9f5b40cbb69a34aa99691a5cc25e4
Tom Rismeyer <trismeyer@gmail.com>
2015-11-11 22:24:52 -0600

    Combat Formulas
    Added HUD file
    Deleted Klogger file
    Updated Unity Project Settings
    Added Formulas in FetchQuestFormulas.txt

---
+++ FetchQuest/Assets/Scripts/UI/HUD.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    void Awake()
+    {
+        if (tag!="Player")
+        {
+            tag = "Player";
+            Debug.LogWarning("PlayerMove script assigned to object without the tag 'Player', tag has been assigned automatically", transform);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        //Obtain the stats that are displayed on screen
+        GUIText[] stats = FindObjectsOfType<GUIText>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
