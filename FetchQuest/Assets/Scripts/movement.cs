6eec4d1b65ee52418d2dbff4ae6a46bd136c5792
AsewAsew <burkdt03@uwgb.edu>
2015-12-03 08:12:35 -0600

    movement update
    Added a jump feature (spacebar) that you can jump only off of objects
    with a tag "Floor"

--- FetchQuest/Assets/Scripts/movement.cs
+++ FetchQuest/Assets/Scripts/movement.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class movement : MonoBehaviour {
 	private Rigidbody rb;
-	public float moveSpeed = .75f;
+	public float moveSpeed = .3f;
 	public float turnSpeed = 5f;
+	public float jumpHeight = 8f;
+
+	private bool isGrounded = true;
 
 	// Use this for initialization
 	void Start () {
@@ -18,8 +21,18 @@ public class movement : MonoBehaviour {
 		float moveVertical = Input.GetAxisRaw ("Vertical");
 		transform.Translate (0.0f, 0.0f, moveVertical * moveSpeed, Space.Self);
 		transform.Rotate (0.0f, turnHorizontal * turnSpeed, 0.0f);
-		if ((transform.rotation.eulerAngles.x != 0.0f) ||(transform.rotation.eulerAngles.x != 0.0f)) {
-			transform.eulerAngles = new Vector3(0.0f, transform.rotation.eulerAngles.y, 0.0f);
+		if ((transform.rotation.eulerAngles.x != 0.0f) || (transform.rotation.eulerAngles.x != 0.0f)) {
+			transform.eulerAngles = new Vector3 (0.0f, transform.rotation.eulerAngles.y, 0.0f);
+		}
+		if (Input.GetButton ("Jump") && isGrounded) {
+			isGrounded = false;
+			rb.velocity = new Vector3(0.0f, jumpHeight, 0.0f);
+		}
+	}
+
+	void OnCollisionEnter(Collision floor){
+		if (floor.collider.tag == "Floor") {
+			isGrounded = true;
 		}
 	}
 }
