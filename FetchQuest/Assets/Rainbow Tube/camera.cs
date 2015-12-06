f7e90331e62458053f564aaa28f1b32da2ae0be0
AsewAsew <burkdt03@uwgb.edu>
2015-12-05 22:09:12 -0600

    Added Rainbow Tunnel & Possible Elevator Script
    I made a rainbow tunnel (I was bored and wanted to do something and just
    because - don't judge). Also created a script that could work as an
    elevator.

---
+++ FetchQuest/Assets/Rainbow Tube/camera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour {
+    public float rotateSpeed = 4f;
+    private float spin = 0f;
+	
+	// Update is called once per frame
+	void Update () {
+        spin++;
+        transform.eulerAngles = new Vector3(0.0f, transform.rotation.y + (spin * rotateSpeed), 0.0f);
+	}
+}
---
+++ FetchQuest/Assets/Rainbow Tube/elevator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class elevator : MonoBehaviour {
+    public GameObject ball;
+    public float dropSpeed = 1f;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent < Rigidbody > ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(ball.transform.position.y <= 15.5)
+        {
+            transform.position = new Vector3(0.0f, 23.75f, 0.0f);
+            ball.transform.position = new Vector3(0.0f, 24.5f, 0.0f);
+        }
+
+        rb.velocity = new Vector3 (0.0f, dropSpeed, 0.0f);
+	}
+}
