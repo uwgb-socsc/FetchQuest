98702a4b0a13f28efbbd6cd752f6b5cfc47c9e16
Dylan Burk <burkdt03@uwgb.edu>
2016-02-15 15:53:00 -0600

    Movement Update 2.1
    Added the Cursor.lockstate functionality, i.e. mouse is locked to center
    but it allows you to look around freely with mouse. It still might be a
    little jumpy

--- FetchQuest/Assets/Scripts/movementRevised.cs
+++ FetchQuest/Assets/Scripts/movementRevised.cs
@@ -2,37 +2,35 @@
 using System.Collections;
 
 [RequireComponent(typeof(CharacterController))]
-public class movementRevised : MonoBehaviour {
+public class movementRevised : MonoBehaviour
+{
     public float speed = 7.5f;
     public float jumpSpeed = 15.0f;
     public float gravity = 20.0f;
     public int escaped = 0;
     private Vector3 moveDirection = Vector3.zero;
-    private float mouseX = 0.0f;
+    private bool isLocked = true;
+    private float mouseX = 0;
     private float mouseY = 0.0f;
+    private float xChange = 0;
+    private float yChange = 0;
+    private int lockDelay = 0;
+    private float prevMouseY = 0.0f;
 
-    void Update () {
+    void Update()
+    {
         CharacterController controller = GetComponent<CharacterController>();
 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            escaped++;
+        }
+
+        Escape();
         SpeedTest();
         Moving();
         Rotator();
-    }
-
-    void SpeedTest()
-    {
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            speed = 3.75f;
-        }
-        else if (Input.GetKey(KeyCode.Q))
-        {
-            speed = 15.0f;
-        }
-        else
-        {
-            speed = 7.5f;
-        }
+        Debug.Log(xChange);
     }
 
     void Moving()
@@ -52,29 +50,121 @@ public class movementRevised : MonoBehaviour {
         controller.Move(moveDirection * Time.deltaTime);
     }
 
+    void Escape()
+    {
+        if (escaped == 1)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            if (lockDelay == 1)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                lockDelay--;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                lockDelay++;
+            }
+            Cursor.visible = false;
+            escaped = 0;
+        }
+
+    }
+
+    void SpeedTest()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 3.75f;
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            speed = 15.0f;
+        }
+        else
+        {
+            speed = 7.5f;
+        }
+    }
+
     void Rotator()
     {
-        mouseX = (Input.mousePosition.x - Screen.width / 2) / 2 + 0.5f;
-        mouseY = -(Input.mousePosition.y - Screen.height / 2) / 2 + 9f;
-        if (mouseX > 180)
+        if (escaped == 1)
         {
-            mouseX -= 360;
-        }
-        else if (mouseX < -180)
-        {
-            mouseX += 360;
-        }
-        transform.eulerAngles = new Vector3(0.0f, mouseX, 0.0f);
+            mouseX = (Input.mousePosition.x - Screen.width / 2) / 2 + 0.5f;
+            mouseY = -(Input.mousePosition.y - Screen.height / 2) / 2 + 16.5f;
+            if (mouseX > 180)
+            {
+                mouseX -= 360;
+            }
+            else if (mouseX < -180)
+            {
+                mouseX += 360;
+            }
+            transform.eulerAngles = new Vector3(0.0f, mouseX, 0.0f);
 
-        if (mouseY < -70)
-        {
-            mouseY = -70;
-        }
-        else if (mouseY > 90)
-        {
-            mouseY = 90;
-        }
+            if (mouseY < -70)
+            {
+                mouseY = -70;
+            }
+            else if (mouseY > 90)
+            {
+                mouseY = 90;
+            }
 
-        Camera.main.transform.eulerAngles = new Vector3(mouseY, mouseX, 0.0f);
+            Camera.main.transform.eulerAngles = new Vector3(mouseY, mouseX, 0.0f);
+        }
+        else
+        {
+            yChange = Mathf.Floor(Input.mousePosition.y - Screen.height / 2 - 16.5f);
+            xChange = Mathf.Floor(Input.mousePosition.x - Screen.width / 2);
+            if (xChange == 1 || xChange == -1 || xChange == -2 || xChange == 2)
+            {
+                mouseX += 0;
+            }
+            else
+            {
+                mouseX += Mathf.Floor(Input.mousePosition.x - Screen.width / 2);
+            }
+            if (yChange == -1 || yChange == 1)
+            {
+                mouseY -= 0;
+            }
+            else
+            {
+                mouseY -= Mathf.Floor(Input.mousePosition.y - Screen.height / 2 - 16.5f);
+            }
+
+            if (mouseX > 180)
+            {
+                mouseX -= 360;
+            }
+            else if (mouseX < -180)
+            {
+                mouseX += 360;
+            }
+            transform.eulerAngles = new Vector3(0.0f, mouseX, 0.0f);
+
+            if (mouseY < -70)
+            {
+                mouseY = -70;
+            }
+            else if (mouseY > 90)
+            {
+                mouseY = 90;
+            }
+
+
+            Camera.main.transform.eulerAngles = new Vector3(mouseY, mouseX, 0.0f);
+
+            if (lockDelay == 1)
+            {
+                prevMouseY = mouseY;
+            }
+        }
     }
 }
