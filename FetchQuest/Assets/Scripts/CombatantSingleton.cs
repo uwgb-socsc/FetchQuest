ff644cd7d05d30ac24dc0d75fbf3d0b934383b20
PepsiDragon <rainick987@yahoo.com>
2016-04-04 16:36:12 -0500

    The CombatantSingleton now exists
    This class will provide the functionality to transfer information
    between the overworld and the combat scene. This will be important in
    setting up the combat scene

---
+++ FetchQuest/Assets/Scripts/CombatantSingleton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CombatantSingleton {
+
+    //use this to transfer data fromt he overworld to the combat scene
+    /*
+    For example:
+        Characters and their information
+        inventory
+        special conditions
+    Singleton's maintain a single instance so that the data will be consistent no matter where you get
+    it from
+    */
+    public List<Combatant> combatants { get; set; } //set this before going into combatants get it when loading combat scene.
+    private static CombatantSingleton instance = null;
+    private CombatantSingleton()
+    {
+    
+    }
+    static public CombatantSingleton getInstance()
+    {
+        if(instance == null)
+        {
+            instance = new CombatantSingleton();
+        }
+        return instance;
+    }
+}
