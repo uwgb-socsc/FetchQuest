228ee94740bfa0b4b4da6bae2f3e5d89e39ce9b9
PepsiDragon <rainick987@yahoo.com>
2016-04-04 17:48:42 -0500

    CombatActionExecutor
    This is the script that will handle the effects and animations for
    combat actions.

---
+++ FetchQuest/Assets/Scripts/Combat/CombatActionExecutor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CombatActionExecutor : MonoBehaviour {
+
+    // Use this for initialization
+    List<CombatAction> actions;
+    int index; //tells what action is currently being exectued
+    bool doneWithIndex; //tells if the action is done being executed (so it moves on to the next one)
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void setActionList(List<CombatAction> actions)
+    {
+        this.actions = actions;
+    }
+}
